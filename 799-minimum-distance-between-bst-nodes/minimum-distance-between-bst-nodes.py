33e04d69ad91fcc65ef847cc2a58a0431dbbf109
GaganPunjabi <45024902+GaganPunjabi@users.noreply.github.com>
2024-01-28 01:00:37 +0530

    Time: 36 ms (70.48%) | Memory: 16.5 MB (59.47%) - LeetSync

--- 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.py
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.py
@@ -5,30 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def predecessor(self, root):
-        root = root.left
-        while root and root.right:
-            root = root.right
-        return root
-
-    def successor(self, root):
-        root = root.right
-        while root and root.left:
-            root = root.left
-        return root
-
+    def inorder(self, root):
+        if root:
+            return self.inorder(root.left) + [root.val] + self.inorder(root.right)
+        else:
+            return []
     def minDiffInBST(self, root: Optional[TreeNode]) -> int:
-        min_value = float('inf')
-        def helper(root):
-            nonlocal min_value
-            if not root:
-                return min_value
-            pred = self.predecessor(root)
-            successor = self.successor(root)
-            if pred and root.val - pred.val < min_value:
-                min_value = root.val - pred.val
-            if successor and successor.val - root.val < min_value:
-                min_value = successor.val - root.val
-            return min(helper(root.left), helper(root.right))
-        helper(root)
-        return min_value
+        min_val = float('inf')
+        lst = self.inorder(root)
+        for i in range(len(lst) - 1):
+            min_val = min(min_val, lst[i+1] - lst[i])
+        return min_val
+        
