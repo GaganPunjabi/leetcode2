0e261b1b759f8c755248ef25331cc02cdbb31a1a
GaganPunjabi <45024902+GaganPunjabi@users.noreply.github.com>
2024-01-28 16:23:08 +0530

    Time: 526 ms (81.17%) | Memory: 51.7 MB (98.75%) - LeetSync

---
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def isLeaf(self, vertex: int) -> bool:
+        return len(self.tm[vertex]) == 1
+
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        if n == 1:
+            return 0
+        # Create adjency matrix
+        self.adj = [[] for i in range(n)]
+        for parent, child in edges:
+            self.adj[parent].append(child)
+            self.adj[child].append(parent)
+        print(self.adj)
+        def dfs(cur: int, parent: int) -> int:
+            time = 0
+            for child in self.adj[cur]:
+                if child == parent:
+                    continue
+                child_time = dfs(child, cur)
+                if child_time or hasApple[child]:
+                    time += child_time + 2
+            return time
+        return dfs(0, -1)
