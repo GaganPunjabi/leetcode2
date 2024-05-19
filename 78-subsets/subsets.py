0c293768ad56dbc1c7fe7a7e0981d4f95138b383
GaganPunjabi <45024902+GaganPunjabi@users.noreply.github.com>
2024-05-19 15:13:08 +0530

    Time: 45 ms (8.62%) | Memory: 16.6 MB (77.68%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,11 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        output = []
-
-        for i in range(2**n, 2 ** (n + 1)):
-            bitmask = bin(i)[3:]
-            output.append([nums[j] for j in range(n) if bitmask[j] == "1"])
-        return output
-    
-# 1000, 1001, 1010, 1011, 1100, 1101, 1110, 1111
+        res = []
+        subset = []
+        
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            subset.append(nums[i])
+            dfs(i+1)
+            subset.pop()
+            dfs(i+1)
+        dfs(0)
+        return res
